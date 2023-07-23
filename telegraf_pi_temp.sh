ef108fb9b49f50fd209eeaf0d4b22f19fb3c2211
Maciej <nowakowskimaciej01@gmail.com>
2023-07-23 15:59:38 +0200

    Support vcgencmd new location (#7)
    * Support vcgencmd new location
    
    * Fix typo

--- telegraf_pi_temp.sh
+++ telegraf_pi_temp.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 prefix="temp="
 suffix="'C"
-gpu=$(/opt/vc/bin/vcgencmd measure_temp)
+vcgencmd_bin=$(/usr/bin/which vcgencmd)
+gpu=$($vcgencmd_bin measure_temp)
 gpu_temp=${gpu#$prefix}
 gpu_temp=${gpu_temp%$suffix}
 echo -e "{\"cpu\":"$(</sys/class/thermal/thermal_zone0/temp)", \"gpu\":$gpu_temp}"
